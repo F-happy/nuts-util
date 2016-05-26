f5da6448b8662cf0962b22d5f24804e734e09c3e
jonnyf <fuhuixiang123@vip.qq.com>
2016-05-26 15:40:52 +0800

    uplib

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /**
- * Created by fuhuixiang on 16/5/26.
+ * Created by fuhuixiang on 16-5-26.
  */
 'use strict';
 module.exports = {
-    jsonp: require('./lib/jsonp')
+    jsonp: require('./lib/jsonp'),
+    getURL: require('./lib/getURL'),
+    cleanLayout: require('./lib/cleanLayout'),
+    addCNZZ: require('./lib/addCNZZ')
 };
